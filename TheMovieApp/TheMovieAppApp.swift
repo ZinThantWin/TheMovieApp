53bcc148edb2e05dd59b2d996446cb5271222a8e
ZinThantWin <zinthantwin1997@gmail.com>
2024-06-15 10:01:56 +0630

    Initial Commit

---
+++ TheMovieApp/TheMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieAppApp.swift
+//  TheMovieApp
+//
+//  Created by ကင်ဇို on 15/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
